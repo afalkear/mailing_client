3d194e44c92893867c4b9b06d8dc8f193e4253f0
Alex Falke <afalkear@gmail.com>
2013-07-08 11:53:03 -0300

    [#51813213] send schedule ID and template ID

--- app/models/mailing/scheduled_mail.rb
+++ app/models/mailing/scheduled_mail.rb
@@ -4,17 +4,16 @@ module Mailing
     include MailingClient::ServiceConfiguration
 
     set_resource_path '/api/v0/scheduled_mails'
+    set_api_key "app_key", Mailing::API_KEY
 
+    attribute :id                          # The schedule ID
+    attribute :template_id                 # The template ID
     attribute :recipient_email             # The person who is receiving the email
     attribute :send_at                     # When the email has been sent or will be sent
     attribute :delivered_at                # When the email has been delivered
     attribute :contact_id                  # The padma-contact id
     attribute :username                    # The username who sends the mail
     attribute :template_name               # The template name
-
-    self.use_api_key = true
-    self.api_key_name = "app_key"
-    self.api_key = Mailing::API_KEY
     
   end
 end
